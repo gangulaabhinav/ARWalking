c94e4e2ad8f4782b405e28b6df7b4ecc82264b59
Abhinav Gangula <abgangul@microsoft.com>
2022-06-29 16:36:24 +0530

    Update ARWalkingView.swift
    Make ARViewContainer class to implement protocol ARSessionDelegate.
    Adding Plane entities from session didAdd anchor protocol method.

--- ARWalking/ARWalkingView.swift
+++ ARWalking/ARWalkingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ARWalkingView.swift
 //  ARWalking
 //
 //  Created by Abhinav Gangula on 27/06/22.
@@ -15,35 +15,54 @@ struct ARWalkingView : View {
     }
 }
 
-struct ARViewContainer: UIViewRepresentable {
+final class ARViewContainer: NSObject, UIViewRepresentable, ARSessionDelegate {
+    let arView: ARView = ARView(frame: .zero)
+
+    override init() {
+        super.init()
+        arView.session.delegate = self
+    }
     
     func makeUIView(context: Context) -> ARView {
-        let arView = ARView(frame: .zero)
-
-        // configure horizontal lane detection
+        // configure plane detection
         arView.automaticallyConfigureSession = true
         let configuration = ARWorldTrackingConfiguration()
-        configuration.planeDetection = [.horizontal]
+        configuration.planeDetection = [.horizontal, .vertical]
         configuration.environmentTexturing = .automatic
         arView.session.run(configuration)
-
-        // add plane mesh model anchor to arView
-        let boxMesh: MeshResource = .generatePlane(width: 0.5, depth: 0.5)
-        let modelEntity = ModelEntity(mesh: boxMesh)
-        let planeAnchor = AnchorEntity(.plane([.any],
-                              classification: [.any],
-                               minimumBounds: [0.5, 0.5]))
-        planeAnchor.addChild(modelEntity)
-        arView.scene.anchors.append(planeAnchor)
-
         return arView
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {
+        print("updateUIView!")
+        return
     }
-    
-    func startHorizontalPlaneDetectionInView(arView: ARView) {
-        
+
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        print("frame updates")
+    }
+
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        print("anchor added")
+        for anchor in anchors {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                if planeAnchor.alignment == .horizontal {
+                    let boxMesh: MeshResource = .generatePlane(width: 0.5, depth: 0.5)
+                    let modelEntity = ModelEntity(mesh: boxMesh)
+                    let planeAnchorEntity = AnchorEntity(anchor: planeAnchor)
+                    planeAnchorEntity.addChild(modelEntity)
+                    arView.scene.anchors.append(planeAnchorEntity)
+                }
+            }
+        }
+    }
+
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        print("anchor updated")
+    }
+
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        print("anchor removed")
     }
 }
 
