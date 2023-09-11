edb8377f22a30fe1b23fc68ba45458553b2f8127
Abhinav Gangula <gangulaabhinav@gmail.com>
2023-09-11 23:46:06 +0530

    Display default ARWalkingZone in ImmersiveView

--- Apple/visionARWalking/ImmersiveView.swift
+++ Apple/visionARWalking/ImmersiveView.swift
@@ -10,21 +10,12 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
+    private let walkingZone = ARWalkingZone()
+
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(immersiveContentEntity)
-
-                // Add an ImageBasedLight for the immersive content
-                guard let resource = try? await EnvironmentResource(named: "ImageBasedLight") else { return }
-                let iblComponent = ImageBasedLightComponent(source: .single(resource), intensityExponent: 0.25)
-                immersiveContentEntity.components.set(iblComponent)
-                immersiveContentEntity.components.set(ImageBasedLightReceiverComponent(imageBasedLight: immersiveContentEntity))
-
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
-            }
+            content.add(walkingZone.walkingZoneEntity)
+        } update: { content in
         }
     }
 }
